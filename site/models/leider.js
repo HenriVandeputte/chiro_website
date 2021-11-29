f29be4c71252fc36b09bffab4a3cdb638a090432
Henri Vandeputte <Vandeputtehenri@gmail.com>
2021-11-29 23:16:32 +0100

    fdgs

--- site/models/leider.js
+++ site/models/leider.js
@@ -13,4 +13,5 @@ var LeiderSchema = new Schema(
 );
 
 
+
 module.exports = mongoose.model('Leider', LeiderSchema);
--- site/routes/groepen.js
+++ site/routes/groepen.js
@@ -6,4 +6,6 @@ router.get('/', function(req, res, next) {
     res.render('groepen.pug', {title : 'Evenementen'});
 });
 
+//////////////////////////////////////////////////////
+
 module.exports = router;
