778c50b29e5bf3eea7d90ccd5c17e756c7850498
Henri Vandeputte <Vandeputtehenri@gmail.com>
2021-11-29 19:03:01 +0100

    dsfqs
    hzejrh

--- site/models/groep.js
+++ site/models/groep.js
@@ -6,13 +6,12 @@ var GroepSchema = new Schema(
     {
         naam: {type: String, required: true},
         leeftijd: {type: Array, required: true},
-        leider: [{type: Schema.Types.ObjectId, ref: 'Leider', required: true}],
-        leden: [{type: Schema.Types.ObjectId, ref: 'Lid', required:true}],
-        beschrijving: [{type: String, required: true}]
+        leider: [{type: Schema.Types.ObjectId, ref: 'Leider'}],
+        leden: [{type: Schema.Types.ObjectId, ref: 'Lid'}],
     }
 )
 
-
+var Kwiks = new GroepSchema({ name: Kwiks, leeftijd: [10,11,12], leider})
 
 
 
--- site/models/lid.js
+++ site/models/lid.js
@@ -6,7 +6,6 @@ var LidSchema = new Schema(
     {
         naam: {type: String, required: true},
         leeftijd: {type: Number, required: true},
-        groep: [{type: Schema.Types.ObjectId, ref: 'Groep', required: true}]
     }
 );
 
