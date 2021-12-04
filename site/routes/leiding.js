bd9f6009f6e5d31bdfba59311a5e995850a5b43d
Henri Vandeputte <Vandeputtehenri@gmail.com>
2021-12-04 17:32:21 +0100

    Leidingpagina realistischer gemaakt

--- site/routes/leiding.js
+++ site/routes/leiding.js
@@ -1,9 +1,15 @@
 var express = require('express');
+const Groep = require("../models/groep");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('leiding.pug');
+    Groep.find({}).sort({orde : 1}).populate('leiding').exec(function (err, list_groepen) {
+        if (err) { return next(err); }
+        //succesful, so render
+
+        res.render('leiding', {groep_list: list_groepen});
+    });
 });
 
 module.exports = router;
